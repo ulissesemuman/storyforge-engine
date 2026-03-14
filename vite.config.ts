29f57411a23753ff47aa8cb71780eef448273c30
Ulisses Farias da Silva Junior <ulissesfarias@live.com>
2026-03-14 19:47:05 -0300

    Movimentação da propriedade base parfa raiz

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/storyforge-engine/',
   server: {
     host: "::",
     port: 8080,
-    base: '/storyforge-engine/',
     hmr: {
       overlay: false,
     },
