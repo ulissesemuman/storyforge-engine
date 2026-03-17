e5b14032a759e405d2ffc0804a9ccfce5dd99806
Ulisses Farias da Silva Junior <ulissesfarias@live.com>
2026-03-17 17:33:15 -0300

    Correção 2 da página 404

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/storyforge-engine">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/biblioteca" element={<Library />} />
